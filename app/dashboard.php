e5ffb19bc7b5814dad88e9d5b12ed2fcd5c8bdd8
Thais Lins <thaislin@gmail.com>
2018-11-11 00:55:40 -0300

    Create layout`

---
+++ app/dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link href="/styles/styles.css" rel="stylesheet">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+</head>
+<body>
+	<!-- REGISTRATION FORM -->
+	<div class="text-center" style="padding:50px 0">
+		<div class="logo">comments</div>
+		<!-- Main Form -->
+		<div class="login-form-1">
+			<form id="register-form" class="text-left">
+				<div class="main-login-form">
+					<div class="login-group">
+						<div class="form-group">
+							<label for="reg_username" class="sr-only">Username</label>
+							<input type="text" class="form-control" id="reg_username" name="reg_username" placeholder="write a comment">
+						</div>
+					
+					</div>
+					<button type="submit" class="comment-button"><i class="fa fa-chevron-right"></i></button>
+				</div>
+			</form>
+		</div>
+		<!-- end:Main Form -->
+	</div>
+</body>
+</html>
