60acef5e6c082deeb4a0f309126eba409294af9f
Elton Viana <eltonvs@outlook.com>
2018-11-17 05:08:13 -0300

    Move config to includes folder and update login.php
    - Reindent lines

--- app/includes/config.php
+++ app/includes/config.php
@@ -4,46 +4,37 @@ function create_connection() {
 	$servername = "mysql";
 	$username = "admin";
 	$password = "admin";
+	$database = 'pocs_security';
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password) or die("Connection failed: " . $conn->connect_error);
+	mysqli_select_db($conn, $database);
 
 	return $conn;
 }
 
 function register_user($name, $username, $password, $conf_password) {
-	$conn = create_connection();
-	$database = 'pocs_security';
-
 	if ($password != $conf_password) {
 		return 'Passwords don\'t match';
 	}
 
 	$sql = "INSERT INTO user (name, username, password) VALUES ('$name', '$username', '$password')";
 
-  mysqli_select_db($conn, $database);
+	$conn = create_connection();
+	$query_exec = mysqli_query($conn, $sql);
+	$error_msg = $query_exec ? '' : mysqli_error($conn) . "<br>Query: $sql";
+	mysqli_close($conn);
 
-  $query_exec = mysqli_query($conn, $sql);
-  $errors = mysqli_error($conn);
-  mysqli_close($conn);
-
-  return $query_exec ? '' : "Error on $sql<br>$errors";
+	return $error_msg;
 }
 
 function user_login($username, $password) {
-	$conn = create_connection();
-	$database = 'pocs_security';
-
 	$sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
 
-    mysqli_select_db($conn, $database);
-    $check = mysqli_query($conn, $sql) or die("erro ao selecionar");
+	$conn = create_connection();
+	$check = mysqli_query($conn, $sql) or die("erro ao selecionar");
+	$error_msg = mysqli_num_rows($check) <= 0 ? 'Invalid user or password' : '';
+	mysqli_close($conn);
 
-    if (mysqli_num_rows($check) <= 0) {
-    	echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        return false;
-    }
-
-    mysqli_close($conn);
-    return true;
+	return $error_msg;
 }
--- app/index.php
+++ app/index.php
@@ -1,44 +1,49 @@
 <?php
+require_once('includes/config.php');
+
 $page = 'Login';
-include('layout/headers.php');
+$error = '';
 
 $username = $_GET['lg_username'];
 $password = MD5($_GET['lg_password']);
 
 if ($username and $password) {
-    require_once('config.php');
-    if (user_login($username, $password)) {
-        header("location:dashboard.php");
-        exit();
-    }
+	$error = user_login($username, $password);
+	if (!$error) {
+		header('Location: dashboard.php');
+		exit();
+	}
 }
+
+include('layout/headers.php');
 ?>
 
 <div class="text-center" style="padding:50px 0">
-    <div class="logo">login</div>
-    <!-- Main Form -->
-    <div class="login-form-1">
-        <form action="dashboard.php" id="login-form" class="text-left">
-            <div class="login-form-main-message"></div>
-            <div class="main-login-form">
-                <div class="login-group">
-                    <div class="form-group">
-                        <label for="lg_username" class="sr-only">Username</label>
-                        <input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
-                    </div>
-                    <div class="form-group">
-                        <label for="lg_password" class="sr-only">Password</label>
-                        <input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
-                    </div>
-                </div>
-                <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
-            </div>
-            <div class="etc-login-form">
-                <p>new user? <a href="signup.php">create new account</a></p>
-            </div>
-        </form>
-    </div>
-    <!-- end:Main Form -->
+	<div class="logo">login</div>
+	<!-- Main Form -->
+	<div class="login-form-1">
+		<form id="login-form" class="text-left">
+			<div class="login-form-main-message"></div>
+			<div class="main-login-form">
+				<?= $error ?>
+				<div class="login-group">
+					<div class="form-group">
+						<label for="lg_username" class="sr-only">Username</label>
+						<input type="text" class="form-control" id="lg_username" name="lg_username" placeholder="username">
+					</div>
+					<div class="form-group">
+						<label for="lg_password" class="sr-only">Password</label>
+						<input type="password" class="form-control" id="lg_password" name="lg_password" placeholder="password">
+					</div>
+				</div>
+				<button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+			</div>
+			<div class="etc-login-form">
+				<p>new user? <a href="signup.php">create new account</a></p>
+			</div>
+		</form>
+	</div>
+	<!-- end:Main Form -->
 </div>
 
 <?php include('layout/footer.php') ?>
